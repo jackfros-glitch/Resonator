f1668c6c50fe116fc2486207eb4ae4318366ddc9
manasseh <hirekaanm3@gmail.com>
2023-11-17 02:21:06 +0100

    project setup.

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Resonator",
+  description:
+    "We collaborate with ambitious brands and people; let’s build something great together.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
